d8337601b8cdcc58d2d5911c9d354eab6adc1d94
Alan Sanchez <sanchezpineda03@gmail.com>
2016-09-07 23:23:04 -0500

    Modified the_excerpt() length

--- functions.php
+++ functions.php
@@ -200,6 +200,13 @@ function shoreditch_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'shoreditch_scripts' );
 
+
+
+function wpdocs_custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 100 );
+
 /**
  * Implement the Custom Header feature.
  */
