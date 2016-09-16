76b03ecc4c5a1168d7d0fc86a2ff0f14b99b5353
Alan Sanchez <sanchezpineda03@gmail.com>
2016-09-16 12:10:06 -0500

    Added email validation in newsletter form.

--- js/newsletter.js
+++ js/newsletter.js
@@ -4,8 +4,14 @@ $(function() {
       url: 'https://miemma.com/suscribe', 
       data: $('#newsletter-form').serialize(),
       beforeSend: function () {
-        $('#newsletter-button').addClass('disabled');
-        $('#newsletter-label').text('');
+        var isEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+        if (isEmail.test($('#newsletter-email').val())) {
+          $('#newsletter-button').addClass('disabled');
+          $('#newsletter-label').text('');
+        } else {
+          $('#newsletter-label').text('Introduce una dirección de correo válida.');
+          return false;
+        }
       },
       success: function () {
         if (data.status === 'subscribed') {
