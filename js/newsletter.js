38e913ada5052e3e4d123b23338aa544eda27a7f
Alan Sanchez <sanchezpineda03@gmail.com>
2016-09-16 17:53:35 -0500

    Fixed ajax url.

--- js/newsletter.js
+++ js/newsletter.js
@@ -2,7 +2,7 @@ $(function() {
   $('#newsletter-button').click(function (e) {
     $.ajax({
       type: 'POST',
-      url: 'https://miemma.com/subscribe', 
+      url: 'https://miemma.com/subscribe/', 
       data: $('#newsletter-form').serialize(),
       beforeSend: function () {
         var isEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
